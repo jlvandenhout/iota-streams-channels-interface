f5a35f6bb877574d8963b40b45178bf52c9e3f2f
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-05-16 13:27:28 +0200

    Reduce problem to send_announce call

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,62 @@
-mod handle;
-mod model;
-mod server;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use warp::Filter;
+use warp::http::StatusCode;
+
+use iota_streams::{
+    app::transport::tangle::{
+        client::Client,
+        PAYLOAD_BYTES,
+    },
+    app_channels::api::tangle::Author,
+};
+
 
 #[tokio::main]
 async fn main() {
-    let app = clap::app_from_crate!()
-        .arg(
-            clap::Arg::new("port")
-                .short('p')
-                .long("port")
-                .about("The port to serve from")
-                .takes_value(true),
-        );
-    let matches = app.get_matches();
+    let author = Arc::new(
+        Mutex::new(
+            Author::new(
+                "AUTHORSEED",
+                "utf-8",
+                PAYLOAD_BYTES,
+                true,
+                Client::new_from_url("https://api.lb-0.testnet.chrysalis2.com")
+            )
+        )
+    );
 
-    let port: u16 = matches.value_of_t("port").unwrap_or(3030);
-    server::start(port).await;
+    let announce = warp::get()
+        .and(with_reference(author.clone()))
+        .and_then(send_announce);
+
+    let port = 3030;
+    println!("Serving on: http://localhost:{}", port);
+    warp::serve(announce)
+        .run(([127, 0, 0, 1], port))
+        .await;
+}
+
+
+fn with_reference<T: Clone + Send>(
+    reference: T
+) -> impl Filter<Extract = (T,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || reference.clone())
+}
+
+
+async fn send_announce(
+    author: Arc<Mutex<Author<Client>>>,
+) -> Result<impl warp::Reply, std::convert::Infallible> {
+    let mut author = author.lock().await;
+
+    // BREAKING:
+    match author.send_announce().await {
+        Ok(address) => {
+            Ok(StatusCode::OK)
+        },
+        Err(_) => {
+            Ok(StatusCode::CONFLICT)
+        },
+    }
 }
