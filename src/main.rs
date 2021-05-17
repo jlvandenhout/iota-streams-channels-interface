63a0bb8d2da83acfcf0098b3a9e5df290bb597ee
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-05-17 16:17:22 +0200

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use std::sync::Arc;
+use std::{
+    convert::Infallible,
+    sync::Arc,
+};
+
 use tokio::sync::Mutex;
 use warp::Filter;
-use warp::http::StatusCode;
 
-use iota_streams::{
-    app::transport::tangle::{
-        client::Client,
-        PAYLOAD_BYTES,
-    },
-    app_channels::api::tangle::Author,
+use iota_streams::app_channels::api::tangle::Author;
+use iota_streams::app::transport::tangle::{
+    PAYLOAD_BYTES,
+    client::Client,
 };
 
 
@@ -26,37 +27,29 @@ async fn main() {
         )
     );
 
-    let announce = warp::get()
+    let route = warp::get()
         .and(with_reference(author.clone()))
         .and_then(send_announce);
 
-    let port = 3030;
-    println!("Serving on: http://localhost:{}", port);
-    warp::serve(announce)
-        .run(([127, 0, 0, 1], port))
-        .await;
+    warp::serve(route).run(([127, 0, 0, 1], 3030)).await;
 }
 
 
 fn with_reference<T: Clone + Send>(
     reference: T
-) -> impl Filter<Extract = (T,), Error = std::convert::Infallible> + Clone {
+) -> impl Filter<Extract = (T,), Error = Infallible> + Clone {
     warp::any().map(move || reference.clone())
 }
 
 
 async fn send_announce(
-    author: Arc<Mutex<Author<Client>>>,
-) -> Result<impl warp::Reply, std::convert::Infallible> {
+    author: Arc<Mutex<Author<Client>>>
+) -> Result<impl warp::Reply, Infallible> {
     let mut author = author.lock().await;
 
-    // BREAKING:
-    match author.send_announce().await {
-        Ok(address) => {
-            Ok(StatusCode::OK)
-        },
-        Err(_) => {
-            Ok(StatusCode::CONFLICT)
-        },
+    if let Ok(address) = author.send_announce().await {
+        Ok(address.to_string())
+    } else {
+        Ok(String::from("Failed to send announcement"))
     }
 }
